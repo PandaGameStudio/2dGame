91392060d448b3b2326923fb5d9e204c4981c4de
romanbeaumont07 <romanbeaumont1999@gmail.com>
2024-09-23 20:03:25 -0500

    initial commit

---
+++ player.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+@export var speed = 200
+@export var friction = 0.1
+@export var acceleration = 0.1
+
+func get_input():
+	var input = Vector2()
+	if Input.is_action_pressed('moveRight'):
+		input.x += 1
+	if Input.is_action_pressed('moveLeft'):
+		input.x -= 1
+	if Input.is_action_pressed('moveDown'):
+		input.y += 1
+	if Input.is_action_pressed('moveUp'):
+		input.y -= 1
+	return input
+
+func _physics_process(delta):
+	var direction = get_input()
+	if direction.length() > 0:
+		velocity = velocity.lerp(direction.normalized() * speed, acceleration)
+	else:
+		velocity = velocity.lerp(Vector2.ZERO, friction)
+	move_and_slide()
