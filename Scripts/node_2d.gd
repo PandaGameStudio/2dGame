c08aed16d33745b6d14e2e7d26edca485d586043
romanbeaumont07 <romanbeaumont1999@gmail.com>
2024-09-28 21:31:44 -0500

    Yuh 2.0

--- Scripts/node_2d.gd
+++ Scripts/node_2d.gd
@@ -3,6 +3,3 @@ extends Node2D
 @onready var pauseMenu = $tileMapContainer/Player/pauseMenu
 
 # Called when the node enters the scene tree for the first time.
-func _process(delta):
-	if Input.is_action_pressed("Pause"):
-		pauseMenu.visible = true
